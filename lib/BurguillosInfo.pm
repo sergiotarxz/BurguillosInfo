20da3625105860b8d32567506049ba08e2895c82
sergiotarxz <sergiotarxz@posteo.net>
2022-11-15 23:57:20 +0100

    Adding reverse proxy support.

--- lib/BurguillosInfo.pm
+++ lib/BurguillosInfo.pm
@@ -6,27 +6,32 @@ use Mojo::Base 'Mojolicious', -signatures;
 
 # This method will run once at server start
 sub startup ($self) {
-  my $metrics = BurguillosInfo::Controller::Metrics->new;
-  $self->hook(around_dispatch => sub {
-      my $next = shift;
-      my $c = shift;
-      $metrics->request($c);
-      if (defined $next) {
-	      $next->();
-      }
-  });
-  my $config = $self->plugin('JSONConfig');
-  $self->config(hypnotoad => {listen => ['http://localhost:3000']});
-  # Router
-  my $r = $self->routes;
-  
-  # Normal route to controller
-  $r->get('/')->to('Page#index');
-#  $r->get('/:post')->to('Page#post');
-  $r->get('/<:category>.rss')->to('Page#category_rss');
-  $r->get('/:category')->to('Page#category');
-  $r->get('/posts/<:slug>-preview.png')->to('Page#get_post_preview');
-  $r->get('/posts/:slug')->to('Page#post');
+    my $metrics = BurguillosInfo::Controller::Metrics->new;
+    $self->hook(
+        around_dispatch => sub {
+            my $next = shift;
+            my $c    = shift;
+            $metrics->request($c);
+            if ( defined $next ) {
+                $next->();
+            }
+        }
+    );
+    my $config = $self->plugin('JSONConfig');
+    $self->config(
+        hypnotoad => { proxy => 1, listen => ['http://localhost:3000'] } );
+
+    # Router
+    my $r = $self->routes;
+
+    # Normal route to controller
+    $r->get('/')->to('Page#index');
+
+    #  $r->get('/:post')->to('Page#post');
+    $r->get('/<:category>.rss')->to('Page#category_rss');
+    $r->get('/:category')->to('Page#category');
+    $r->get('/posts/<:slug>-preview.png')->to('Page#get_post_preview');
+    $r->get('/posts/:slug')->to('Page#post');
 }
 
 1;
