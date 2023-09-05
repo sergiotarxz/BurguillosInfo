2d85fc5dc94282326cd5c952a71218e1f2c3ef58
sergiotarxz <sergiotarxz@posteo.net>
2023-09-05 20:28:46 +0200

    Revert "Removing ads for demo."
    This reverts commit e5acf7da30155d067524770955358416b1e661c9.

---
+++ lib/BurguillosInfo/Ads/Afasode.pm
@@ -0,0 +1,39 @@
+package BurguillosInfo::Ads::Afasode;
+
+use v5.36.0;
+
+use strict;
+use warnings;
+use utf8;
+
+use feature 'signatures';
+
+use Moo;
+
+use parent 'BurguillosInfo::Ad';
+
+sub id ($self) {
+    return 'afasode-loteria';
+}
+
+sub weight {
+    return 15;
+}
+
+sub is_active ($self) {
+    return 0;
+}
+
+sub img {
+    return '/img/afasode.svg';
+}
+
+sub href {
+    return '/posts/boletos-loteria-afasode-sevilla-2023';
+}
+
+sub text {
+    return
+'54359 es el número de la lotería de Navidad de AFASODE, colabora con una buena causa y no pierdas la ilusión.';
+}
+1;
