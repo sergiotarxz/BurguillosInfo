d8971679f24115ab6c0c466a7f83aed6127eb3e8
sergiotarxz <sergiotarxz@posteo.net>
2023-09-07 20:00:25 +0200

    pizza = pizzeria

--- lib/BurguillosInfo/IndexUtils.pm
+++ lib/BurguillosInfo/IndexUtils.pm
@@ -25,6 +25,7 @@ sub normalize($self, $text) {
     my $stemmer = Lingua::Stem::Snowball->new( lang => 'es' );
     $stemmer->stem_in_place(\@words);
     $decomposed = join " ", @words;
+    $decomposed =~ s/\bpizzeri\b/pizz/g;
     return $decomposed;
 }
 
