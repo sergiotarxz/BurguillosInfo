615994e81751fc0309c1a7b01589ad32c3607429
Sergiotarxz <sergiotarxz@posteo.net>
2023-09-19 17:47:43 +0200

    Forcing facebook to take whatsapp images.

--- lib/BurguillosInfo/Controller/Page.pm
+++ lib/BurguillosInfo/Controller/Page.pm
@@ -172,6 +172,7 @@ sub get_category_preview {
     }
     my $category    = $categories->{$category_slug};
     my $is_whatsapp = $self->req->headers->user_agent =~ /whatsapp/i;
+    $is_whatsapp ||= $self->req->headers->user_agent =~ /facebook/i;
     $self->render(
         format => 'png',
         data   => $category_model->PreviewOg( $category, $is_whatsapp )
@@ -188,6 +189,7 @@ sub get_post_preview {
         return;
     }
     my $is_whatsapp = $self->req->headers->user_agent =~ /whatsapp/i;
+    $is_whatsapp ||= $self->req->headers->user_agent =~ /facebook/i;
     say $self->req->headers->user_agent;
     my $post = $posts_slug->{$slug};
     $self->render(
