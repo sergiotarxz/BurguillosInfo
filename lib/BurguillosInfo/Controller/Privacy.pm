6d887f7743e3b302a71db9fd5bd202c909664838
Sergiotarxz <sergiotarxz@posteo.net>
2023-11-30 22:24:35 +0100

    Updating privacy policy.

--- lib/BurguillosInfo/Controller/Privacy.pm
+++ lib/BurguillosInfo/Controller/Privacy.pm
@@ -10,10 +10,12 @@ use Mojo::Base 'Mojolicious::Controller', '-signatures';
 
 sub index($self) {
     return $self->render(text => <<"EOF");
-Esta aplicación no almacena datos que puedan identificar
-de forma única a los usuarios.\r\n
 Con fines analíticos y técnicos se almacenan la cantidad
 de visitas a cada página.\r\n
+La dirección IP, Agente de Usuario y parametros GET con los 
+que se visita la página es almacenado de forma temporal 
+(90 días) para detectar posibles ciberataques, tras ese tiempo
+es sustituido por un hash.
 Esta política puede cambiar en un futuro si se
 requieren funcionalidades como registros.\r\n
 EOF
