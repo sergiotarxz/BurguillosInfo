edfb476ed28ed9aa6ce39b0bf5c5ddaaa19a9a43
Sergiotarxz <sergiotarxz@posteo.net>
2023-12-04 15:00:11 +0100

    Allowing google translate to serve ads.

--- lib/BurguillosInfo/Controller/Ads.pm
+++ lib/BurguillosInfo/Controller/Ads.pm
@@ -13,6 +13,7 @@ sub next_ad {
     my $self              = shift;
     my $ads_factory       = BurguillosInfo::Ads->new;
     my $current_ad_number = $self->param('n');
+    $self->res->headers->access_control_allow_origin('*');
     $self->render( json => $ads_factory->get_next($current_ad_number) );
 }
 1;
