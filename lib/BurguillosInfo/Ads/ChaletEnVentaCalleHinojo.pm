ba6f7b418e68b412f9610478878daece2d500052
Sergiotarxz <sergiotarxz@posteo.net>
2024-05-10 11:07:08 +0200

    Adding add chalet.

---
+++ lib/BurguillosInfo/Ads/ChaletEnVentaCalleHinojo.pm
@@ -0,0 +1,50 @@
+package BurguillosInfo::Ads::ChaletEnVentaCalleHinojo;
+
+use v5.36.0;
+
+use strict;
+use warnings;
+use utf8;
+
+use feature 'signatures';
+
+use Moo;
+
+use parent 'BurguillosInfo::Ad';
+
+sub id ($self) {
+    return 'chalet-en-venta-calle-hinojo';
+}
+
+sub weight {
+    return 50;
+}
+
+sub max_alternative {
+    return 1;
+}
+
+sub seconds($self) {
+    return 15;
+}
+
+sub default_alternative($self) {
+    return int($self->alternative * ($self->max_alternative + 1));
+}
+
+sub is_active ($self) {
+    return 1;
+}
+
+sub img ($self) {
+    return '/img/chalet-calle-hinojo.webp';
+}
+
+sub text($self) {
+    return 'Chalet pareado en venta en calle Hinojo por 160 000€';
+}
+
+sub href {
+    return 'https://www.idealista.com/inmueble/104802645/';
+}
+1;
